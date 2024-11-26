f4174053f17f19696b6d49b7fc43097859cc43b2
adrienb39 <adrienb39@yahoo.com>
2024-11-26 12:26:12 +0100

    ajout de la fonction pour afficher les jours et les horraires du tableau dans l'id horraires

--- horraires.js
+++ horraires.js
@@ -1,2 +1,36 @@
 // Horraires d'ouverture
-console.log("horraires d'ouverture")
+console.log("horraires d'ouverture");
+
+const horraires = [
+    { jour: "Lundi", horraire: "9h00-18h00" },
+    { jour: "Mardi", horraire: "9h00-18h00" },
+    { jour: "Mercredi", horraire: "9h00-18h00" },
+    { jour: "Jeudi", horraire: "9h00-18h00" },
+    { jour: "Vendredi", horraire: "9h00-18h00" },
+    { jour: "Samedi", horraire: "9h00-17h00" },
+    { jour: "Dimanche", horraire: "Fermé" }
+];
+
+function afficherHorraires() {
+    const horrairesContainer = document.getElementById('horraires');
+
+    horrairesContainer.innerHTML = '';
+
+    horraires.forEach((horaire) => {
+        const horaireElement = document.createElement('div');
+        horaireElement.classList.add('card-text', 'border-top');
+
+        horaireElement.innerHTML = `
+            <div class="text-warning d-inline">
+                <i class="bi bi-brightness-high-fill"></i>
+                <div class="d-inline text-black">
+                    ${horaire.jour}
+                </div>
+            </div>
+        `;
+
+        horrairesContainer.appendChild(horaireElement);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", afficherHorraires);
